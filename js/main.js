7c06d52fd41964eee1819fc9879d363a55b6d854
Rodrigo Seguel <63627567+Khalzz@users.noreply.github.com>
2020-11-22 12:39:08 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -44,3 +44,65 @@ function openItch(){
 function openGit(){
   window.open("https://github.com/Khalzz");
 }
+
+let opcion = 1
+
+function openNav(){
+  document.querySelector(".navegation").style.height = "50vw";
+  document.querySelector(".navegation").style.padding = "2vw";
+  
+  document.getElementById("button-know").style.display = "block"
+  document.getElementById("button-know").style.height = "9vw"
+  document.getElementById("button-know").style.padding = "2vw 12vw"
+  document.getElementById("button-know").style.margin = "1vw 20vw"
+  
+  document.getElementById("button-find").style.display = "block"
+  document.getElementById("button-find").style.height = "9vw"
+  document.getElementById("button-find").style.padding = "2vw 12vw"
+  document.getElementById("button-find").style.margin = "1vw 20vw"
+  
+  document.getElementById("button-blog").style.display = "block"
+  document.getElementById("button-blog").style.height = "9vw"
+  document.getElementById("button-blog").style.padding = "2vw 12vw"
+  document.getElementById("button-blog").style.margin = "1vw 20vw"
+
+  document.getElementById("button-cont").style.display = "block"
+  document.getElementById("button-cont").style.height = "9vw"
+  document.getElementById("button-cont").style.padding = "2vw 12vw"
+  document.getElementById("button-cont").style.margin = "1vw 20vw"
+  opcion = 2;
+}
+
+function closeNav() {   
+  document.querySelector(".navegation").style.height = "0";
+  document.querySelector(".navegation").style.padding = "0";
+  
+  document.getElementById("button-know").style.height = "0"
+  document.getElementById("button-know").style.padding = "0"
+  document.getElementById("button-know").style.margin = "0"
+  document.getElementById("button-know").style.display = "none"
+  
+  document.getElementById("button-find").style.height = "0"
+  document.getElementById("button-find").style.padding = "0"
+  document.getElementById("button-find").style.margin = "0"
+  document.getElementById("button-find").style.display = "none"
+  
+  document.getElementById("button-blog").style.height = "0"
+  document.getElementById("button-blog").style.padding = "0"
+  document.getElementById("button-blog").style.margin = "0"
+  document.getElementById("button-blog").style.display = "none"
+  
+  document.getElementById("button-cont").style.height = "0"
+  document.getElementById("button-cont").style.padding = "0"
+  document.getElementById("button-cont").style.margin = "0"
+  document.getElementById("button-cont").style.display = "none"
+  opcion = 1;   
+}
+
+function action() {
+  if (opcion == 1) {
+    openNav()
+  } else if (opcion == 2) {
+    closeNav()
+  }
+}
