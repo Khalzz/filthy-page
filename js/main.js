33c794f1a63d1082b0847f7c9ef9831e4d28c6c2
Rodrigo Seguel <fearushb@gmail.com>
2020-11-27 14:51:40 -0300

    update

--- js/main.js
+++ js/main.js
@@ -60,24 +60,28 @@ function openNav(){
   document.querySelector(".navegation").style.padding = "5vw";
   
   document.getElementById("button-know").style.height = "9vw"
+  document.getElementById("button-know").style.width = "50vw"
   document.getElementById("button-know").style.padding = "2vw 12vw"
   document.getElementById("button-know").style.margin = "1vw 20vw"
   document.getElementById("button-know").style.fontSize = "4vw"
   document.getElementById("button-know").style.border = "2px solid #E5E9F0"
   
   document.getElementById("button-find").style.height = "9vw"
+  document.getElementById("button-find").style.width = "50vw"
   document.getElementById("button-find").style.padding = "2vw 12vw"
   document.getElementById("button-find").style.margin = "1vw 20vw"
   document.getElementById("button-find").style.fontSize = "4vw"
   document.getElementById("button-find").style.border = "2px solid #E5E9F0"
   
   document.getElementById("button-blog").style.height = "9vw"
+  document.getElementById("button-blog").style.width = "50vw"
   document.getElementById("button-blog").style.padding = "2vw 12vw"
   document.getElementById("button-blog").style.margin = "1vw 20vw"
   document.getElementById("button-blog").style.fontSize = "4vw"
   document.getElementById("button-blog").style.border = "2px solid #E5E9F0"
 
   document.getElementById("button-cont").style.height = "9vw"
+  document.getElementById("button-cont").style.width = "50vw"
   document.getElementById("button-cont").style.padding = "2vw 12vw"
   document.getElementById("button-cont").style.margin = "1vw 20vw"
   document.getElementById("button-cont").style.fontSize = "4vw"
@@ -90,31 +94,35 @@ function closeNav() {
   document.querySelector(".navegation").style.padding = "0";
   
   document.getElementById("button-know").style.height = "0"
+  document.getElementById("button-know").style.width = "100%"
   document.getElementById("button-know").style.padding = "0"
   document.getElementById("button-know").style.margin = "0"
   document.getElementById("button-know").style.fontSize = "0"
-  document.getElementById("button-know").style.border = "0 solid #ffffff"
+  document.getElementById("button-know").style.border = "0 solid #816877"
   document.getElementById("button-know").style.boxShadow = "0px 0px 0px rgba(0,0,0,0)"
   
   document.getElementById("button-find").style.height = "0"
+  document.getElementById("button-find").style.width = "100%"
   document.getElementById("button-find").style.padding = "0"
   document.getElementById("button-find").style.margin = "0"
   document.getElementById("button-find").style.fontSize = "0"
-  document.getElementById("button-find").style.border = "0 solid #ffffff"
+  document.getElementById("button-find").style.border = "0 solid #816877"
   document.getElementById("button-find").style.boxShadow = "0px 0px 0px rgba(0,0,0,0)"
   
   document.getElementById("button-blog").style.height = "0"
+  document.getElementById("button-blog").style.width = "100%"
   document.getElementById("button-blog").style.padding = "0"
   document.getElementById("button-blog").style.margin = "0"
   document.getElementById("button-blog").style.fontSize = "0"
-  document.getElementById("button-blog").style.border = "0 solid #ffffff"
+  document.getElementById("button-blog").style.border = "0 solid #816877"
   document.getElementById("button-blog").style.boxShadow = "0px 0px 0px rgba(0,0,0,0)"
   
   document.getElementById("button-cont").style.height = "0"
+  document.getElementById("button-cont").style.width = "100%"
   document.getElementById("button-cont").style.padding = "0"
   document.getElementById("button-cont").style.margin = "0"
   document.getElementById("button-cont").style.fontSize = "0"
-  document.getElementById("button-cont").style.border = "0 solid #ffffff"
+  document.getElementById("button-cont").style.border = "0 solid #816877"
   document.getElementById("button-cont").style.boxShadow = "0px 0px 0px rgba(0,0,0,0)"
   opcion = 1;   
 }
