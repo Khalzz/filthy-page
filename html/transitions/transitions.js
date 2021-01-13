7ee71da6292b8cc8bd6cb792a1673c82e99a1961
Rodrigo Seguel <fearushb@gmail.com>
2021-01-13 00:11:32 -0300

    update

--- html/transitions/transitions.js
+++ html/transitions/transitions.js
@@ -1,7 +1,21 @@
 const transitionScuare = document.querySelector('.transition')
+const transitionName = document.querySelector('.transition-name')
+const transitionSubtitle = document.querySelector('.transition-subtitle')
 
-const transitionClose = () => {transitionScuare.style.left = '0'}
-const transitionOpen = () => {transitionScuare.style.left = '200vw'}
+const transitionClose = () => {
+  transitionScuare.style.left = '0'
+  transitionName.style.transition = '0.4s'
+  transitionSubtitle.style.transition = '0.8s'
+  transitionName.style.left = '0';
+  transitionSubtitle.style.left = '0';
+}
+const transitionOpen = () => {
+  transitionScuare.style.left = '200vw';
+  transitionName.style.transition = '0.8s'
+  transitionSubtitle.style.transition = '0.8s'
+  transitionName.style.left = '200vw';
+  transitionSubtitle.style.left = '200vw';
+}
 
 window.onload = () => {
   setTimeout(() => {
