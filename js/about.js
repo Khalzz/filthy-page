0eb1fb6f395f71899fb5bf355a83b20c4a58ee42
Rodrigo Seguel <fearushb@gmail.com>
2021-01-19 01:26:23 -0300

    update

---
+++ js/about.js
@@ -0,0 +1,21 @@
+const backButton = document.querySelector('.back-button')
+
+window.onscroll = () => {
+    let y = window.scrollY;
+    console.log(y);
+    if (y >= 200) {
+        
+        setTimeout(() => {
+            backButton.style.marginTop = "4vh"
+            backButton.style.position = 'fixed'
+          }, 200);
+    } else {
+        setTimeout(() => {
+            backButton.style.marginTop = "-10vh"
+            backButton.style.marginTop = "3vh"
+              }, 200);
+        setTimeout(() => {
+            backButton.style.position = 'absolute'
+              }, 300);
+    }
+}
