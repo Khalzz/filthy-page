931fae586de9681c960583281337ae139e580c5d
Rodrigo Seguel <fearushb@gmail.com>
2021-01-24 18:18:23 -0300

    update

---
+++ js/about.js
@@ -0,0 +1,3 @@
+const gotoFirst = () => window.location.href = '#section-1';
+const gotoSecond = () => window.location.href = '#section-2';
+const gotoThird = () => window.location.href = '#section-3';
