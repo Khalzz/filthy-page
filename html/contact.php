2901d7df19e5c115d81e17cf32da75445c06ffde
Rodrigo Seguel <fearushb@gmail.com>
2021-01-28 01:30:20 -0300

    update

--- html/contact.php
+++ html/contact.php
@@ -3,14 +3,14 @@
 if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $mailFrom = $_POST['email'];
-    $subject = "New Message from your portfolio"
+    $subject = "New Message from your portfolio";
     $message = $_POST['msg'];
 
-    $mailTo = "rseguel.dev@gmail.com":
+    $mailTo = "rseguel.dev@gmail.com";
     $headers = "From: ".$mailFrom;
     $txt = "You have recieved a mail from ".$name.".\n\n".$message;
 
     mail($mailTo, $subject, $txt, $headers,);
-    header("location: index.php?mailsend")
+    header("location: index.php?mailsend");
 }
 ?>
