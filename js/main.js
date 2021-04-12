bac70f3037e3d49062d9a2b0e7c74f403df5c84f
Rodrigo Seguel <63627567+Khalzz@users.noreply.github.com>
2021-04-11 22:56:48 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -38,7 +38,7 @@ var swiper = new Swiper('.swiper-container', {
 const openItch = () => window.open('https://khalzdev.itch.io/');
 const openGit = () => window.open('https://github.com/Khalzz');
 const openStack = () => window.open('https://es.stackoverflow.com/users/175478/khals');
-const openIg = () => window.open('https://www.instagram.com/moon.drawner.chu.p0u/');
+const openIg = () => window.open('https://www.instagram.com/khalz_art/');
 
 const gotoTitle = () => window.location.href = '#section-1';
 const gotoSecond = () => window.location.href = '#section-2';
